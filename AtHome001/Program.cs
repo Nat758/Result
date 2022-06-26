04d26b7d909c346a17a8f142d21eb9b6af7933af
Natalia <natalia.sidorova85@mail.ru>
2022-06-26 19:37:40 +0300

    Задача 2

--- AtHome001/Program.cs
+++ AtHome001/Program.cs
@@ -1,4 +1,17 @@
-﻿int num1 = 5;
-int num2 = 7;
-if (num1>num2 = num1<num2);
-
+﻿Console.WriteLine("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num2 > max) ;
+{
+    max = num2;
+}
+Console.WriteLine("Введите третье число:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num3 > max) ;
+{
+    max = num3;
+}
+Console.Write("max = ");
+Console.Write(max);
