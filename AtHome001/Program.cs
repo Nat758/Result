500736635058c68968267b267ad46161e6ceb434
Natalia <natalia.sidorova85@mail.ru>
2022-06-28 11:13:51 +0300

    Добавила задание AtHome001

--- AtHome001/Program.cs
+++ AtHome001/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите первое число:");
+﻿// Напишите программу, которая принимает на вход три 
+// числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число:");
 int num1 = Convert.ToInt32(Console.ReadLine());
 int max = num1;
 Console.WriteLine("Введите второе число:");
