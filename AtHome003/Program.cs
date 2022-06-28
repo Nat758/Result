c06269a328125b45e14fee95cfbb387899b518ca
Natalia <natalia.sidorova85@mail.ru>
2022-06-28 11:05:44 +0300

    AtHome003

--- AtHome003/Program.cs
+++ AtHome003/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая на вход принимает 
+//  число (N), а на выходе показывает все чётные
+//  числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i <= N)
+{
+    Console.Write(i);
+    i = i + 2;
+}
+
+
+
+
+
