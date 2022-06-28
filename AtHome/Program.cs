558ffc79cb6c3705ccb98cb2e910dcc1c72e6d84
Natalia <natalia.sidorova85@mail.ru>
2022-06-28 11:10:15 +0300

    Добавила задание

--- AtHome/Program.cs
+++ AtHome/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Введите первое число: ");
+﻿//  Напишите программу, которая на вход принимает 
+//  два числа и выдаёт, какое число большее,
+//  а какое меньшее.
+Console.WriteLine("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
