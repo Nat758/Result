5e87965ef2b1d4b4c6f056105d3e29fbed3535bd
Natalia <natalia.sidorova85@mail.ru>
2022-06-28 18:12:12 +0300

    AtHome004

--- AtHome004/Program.cs
+++ AtHome004/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход 
+//  трёхзначное число и на выходе показывает вторую
+//  цифру этого числа.
+int rndnum = new Random(). Next(100, 1000);
+Console.WriteLine($"Случайное число из 100, 999 => {rndnum}");
+int RemSecDigit(int num)
+{
+    int secondDigit = (num % 100) / 10;
+    return secondDigit;
+}
+int res = RemSecDigit(rndnum);
+Console.WriteLine(res);
