c0904f705ec7e79fd42d959a59d66ab77a6c4933
Natalia <natalia.sidorova85@mail.ru>
2022-06-30 16:12:14 +0300

    Add AtHome005

--- AtHome001/Program.cs
+++ AtHome001/Program.cs
@@ -5,13 +5,13 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 int max = num1;
 Console.WriteLine("Введите второе число:");
 int num2 = Convert.ToInt32(Console.ReadLine());
-if (num2 > max) ;
+if (num2 > max) 
 {
     max = num2;
 }
 Console.WriteLine("Введите третье число:");
 int num3 = Convert.ToInt32(Console.ReadLine());
-if (num3 > max) ;
+if (num3 > max) 
 {
     max = num3;
 }
--- AtHome005/Program.cs
+++ AtHome005/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число: ");
+string N = (Console.ReadLine());
+int numLength = N.Length;
+int num = Convert.ToInt32(N);
+if(num<100)
+{
+Console.WriteLine("Третьей цифры нет:");
+}
+else if(num>100 && num<1000)
+{
+    Console.WriteLine(num%10);
+}
+else if (num>=1000)
+{
+    
+    num = num % (int)Math.Pow(10,numLength-1);   
+    num = num % (int)Math.Pow(10,numLength-2);   
+    num = num / (int)Math.Pow(10,numLength-3);
+   Console.WriteLine (num);
+}
