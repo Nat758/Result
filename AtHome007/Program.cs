560bf649621bd8eb1a2913c9a6abd97e6552be6c
Natalia <natalia.sidorova85@mail.ru>
2022-07-01 14:54:48 +0300

    Решение задачи

--- AtHome007/Program.cs
+++ AtHome007/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+Console.WriteLine("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int res = (num1 + num2) % 2;
+if (res == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
