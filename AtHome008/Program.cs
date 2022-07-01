73684fd8bef23fabfca73aeaedcbd5b889901d46
Natalia <natalia.sidorova85@mail.ru>
2022-07-01 20:36:22 +0300

    Решение задачи

--- AtHome008/Program.cs
+++ AtHome008/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int res1 = num % 7;
+int res2 = num %23;
+if(res1==0 && res2==0)
+{
+    Console.WriteLine($"Кратно");
+}  
+
+else
+{
+    Console.WriteLine($"Не кратно, остаток от деления {res1} && {res2}");
+}
