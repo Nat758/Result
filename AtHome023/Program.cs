748fdbf2a4c86344895635a035b0648a58c70941
Natalia <natalia.sidorova85@mail.ru>
2022-07-04 14:14:32 +0300

    Решение задачи

--- AtHome023/Program.cs
+++ AtHome023/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число
+//  (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+void Cube (int a)
+{
+    int index = 1;
+    while(index <= a)
+    {
+        int cube = index*index*index;
+        Console.WriteLine($"{cube} ");
+        index++;
+    }
+}
+Cube(num);
