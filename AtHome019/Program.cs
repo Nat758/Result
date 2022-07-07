76f483542db648d78b148da6c295bc21cb46c755
Natalia <natalia.sidorova85@mail.ru>
2022-07-07 18:09:51 +0300

    AtHome019 Решение

--- AtHome019/Program.cs
+++ AtHome019/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+
+
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+string Palindrom(int digit)
+{
+    string answer = "";
+     int a = digit / 10000; 
+     int b = digit % 10;
+     int c = (digit / 1000) % 10;
+     int d = (digit % 100) / 10;
+     if (a == b && c == d)
+     {
+        answer= "Да";
+     }
+     else
+     {
+        answer="Нет";
+     }
+    return answer;
+}    
+Console.WriteLine(Palindrom(num));
+
+
+
+
