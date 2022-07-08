9d41b0564c6dd65a9ab62d0ad837bc0a1c0d7134
Natalia <natalia.sidorova85@mail.ru>
2022-07-08 20:11:39 +0300

    AtHome025 Решение

--- AtHome025/Program.cs
+++ AtHome025/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите цикл, который принимает на вход два числа (A и B)
+//   и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+int degree = 1;
+for(int i = 1; i<=b; i++)
+{
+    degree=degree*a;
+}
+
+Console.WriteLine($"Результат возведения в степень числа {a} в степень {b}= {degree}:");
---
+++ AtHome026/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Напишите программу, которая принимает на вход число и 
+//  выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <=number; i++)
+{
+    sum = sum+i;
+}
+Console.WriteLine($"Сумма числа {sum}");
