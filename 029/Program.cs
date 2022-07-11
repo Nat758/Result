9c2497359479b673306c63583b67e3ec16b09760
Natalia <natalia.sidorova85@mail.ru>
2022-07-11 14:27:32 +0300

    AtHome029 Решение

--- 029/Program.cs
+++ 029/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//  Напишите программу, которая задаёт массив из 8 элементов, заполненный 
+//  псевдослучайными числами и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+int[] array = new int[8];
+int index = 0;
+var rnd= new Random();
+
+while(index<array.Length)
+{
+    array[index]= rnd.Next(0,100);
+    index++;
+}
+for(int i= 0; i <array.Length;i++)
+{
+    if(i==0) Console.Write("[");
+    if(i<array.Length-1) Console.Write(array[i]+",");
+    else Console.Write(array[i]+"]");
+}
