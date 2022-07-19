dd3b89f59213e6576e96bbfb00cf0475839d8185
Natalia <natalia.sidorova85@mail.ru>
2022-07-19 22:10:07 +0300

    AtHome050 Решение

--- AtHome050/Program.cs
+++ AtHome050/Program.cs
@@ -5,17 +5,22 @@
 // 5 9 2 3
 // 8 4 2 4
 // 1, 7 -> такого числа в массиве нет
-int[,] GreateMatrixRndInt(int m, int n);
-int[,] array = new int[m, n];
-var rnd = new Random();
-for (int i = 0; i < array.GetLength(0); i++)
+
+
+int[,] GreateMatrixRndInt(int m, int n, int min, int max)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
+    int[,] array = new int[m, n];
+    var rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        array[i, j] = rnd.Next(20);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
     }
     return array;
 }
+
 
 void PrintMatrix(int[,] arr)
 {
@@ -23,12 +28,32 @@ void PrintMatrix(int[,] arr)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            if (i == 0) Console.Write("[");
-            if (j <arr.GetLength - 1) Console.Write(arr[j] + ",");
+            if (j == 0) Console.Write("[");
+            if (j < arr.GetLength(1) - 1) Console.Write(arr[i, j] + ",");
             else Console.Write(arr[i, j] + "]");
         }
         Console.WriteLine();
     }
 }
-int[,] matrix = GreateMatrixRndInt(3, 4);
+Console.WriteLine("Введите позицию строки:");
+int index1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите позицию столбца:");
+int index2 = Convert.ToInt32(Console.ReadLine());
+
+void ElementMatrix(int ind1, int ind2, int[,] ind)
+{
+    if(ind1>ind.GetLength(0) || ind2> ind.GetLength(1))
+    {
+    Console.WriteLine("Такого числа нет");
+    }
+    else
+    {
+        Console.WriteLine(ind[ind1,ind2] );
+    }
+}
+
+
+int[,] matrix = GreateMatrixRndInt(3, 4, 1, 9);
 PrintMatrix(matrix);
+Console.WriteLine();
+ElementMatrix(index1,index2,matrix );
