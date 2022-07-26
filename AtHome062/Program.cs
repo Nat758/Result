fb3354ee435f82479414d680dd43a81806d0c5b7
Natalia <natalia.sidorova85@mail.ru>
2022-07-26 16:36:17 +0300

    AtHome062 Решение

--- AtHome062/Program.cs
+++ AtHome062/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Заполните спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+int n = 4;
+int[,] SpiralMatrix(int n)
+{
+    int[,] array = new int[n, n];
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+    while (temp <= array.GetLength(0) * array.GetLength(1))
+    {
+        array[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= array.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > array.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < arr.GetLength(1) - 1) Console.Write(arr[i, j] + ",");
+            else Console.Write(arr[i, j] + "]");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = SpiralMatrix(4);
+PrintMatrix(matrix);
--- AtHome068/Program.cs
+++ AtHome068/Program.cs
@@ -8,12 +8,13 @@ int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int fuctionAckerman = Aсkerman(m,n);
-Console.Write($"{Ackerman}: ");
+Console.Write($"{fuctionAckerman } ");
 
 int Aсkerman(int m,int n)
 {
   if (m == 0) return n + 1;
-  else if (n == 0) return Aсkerman (m - 1, 1);
-  else return Aсkerman(m - 1, Aсkerman(m, n - 1));
+  else if (n == 0 && m>0) return Aсkerman (m - 1, 1);
+  else if(n>0 &&m>0) return Aсkerman(m - 1, Aсkerman(m, n - 1));
+  else return n+1;
 }
 
