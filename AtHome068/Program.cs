13c01a45c392f72ffb70387c3da97caefaabae97
Natalia <natalia.sidorova85@mail.ru>
2022-07-26 12:33:46 +0300

    AtHome062

--- AtHome068/Program.cs
+++ AtHome068/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
+
+
+Console.WriteLine("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int fuctionAckerman = Aсkerman(m,n);
+Console.Write($"{Ackerman}: ");
+
+int Aсkerman(int m,int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Aсkerman (m - 1, 1);
+  else return Aсkerman(m - 1, Aсkerman(m, n - 1));
+}
+
